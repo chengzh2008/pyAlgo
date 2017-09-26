96b424048f5122efec37f4a30798e4f6896ab403
Zhihong Cheng <chengzh2008@gmail.com>
2017-09-25 21:43:06 -0700

    upto Basics

--- tests/test_ch02.py
+++ tests/test_ch02.py
@@ -30,6 +30,42 @@ class Ch02TestSuite(unittest.TestCase):
         ]
         self.assertEqual(b in N[a], True, msg="{0}, {1}".format("is b in N[a]", b in N[a]))
 
+    def test_adjacency_dict_graph(self):
+        a, b, c, d, e, f, g, h = range(8)
+        N = [
+            {b:2, c:1, d:3, e:9, f:4},    # a
+            {c:4, e:3},                   # b
+            {d:8},                        # c
+            {e:7},                        # d
+            {f:5},                        # e
+            {c:2, g:2, h:2},              # f
+            {f:1, h:6},                   # g
+            {f:9, g:8}                    # h
+        ]
+        self.assertEqual(N[a][b], 2, msg="{0}, {1}".format("weight of edge a b", N[a][b]))
+
+    def test_weight_matrix_graph(self):
+        a, b, c, d, e, f, g, h = range(8)
+        inf = float('inf')
+
+        #       a    b    c    d    e    f    g    h
+
+        W = [
+            [  0,   2,   1,   3,   9,   4, inf, inf], # a
+            [inf,   0,   4, inf,   3, inf, inf, inf], # b
+            [inf, inf,   0,   8, inf, inf, inf, inf], # c
+            [inf, inf, inf,   0,   7, inf, inf, inf], # d
+            [inf, inf, inf, inf,   0,   5, inf, inf], # e
+            [inf, inf,   2, inf, inf,   0,   2,   2], # f
+            [inf, inf, inf, inf, inf,   1,   0,   6], # g
+            [inf, inf, inf, inf, inf,   9,   8,   0]
+        ] # h
+        self.assertEqual(W[a][b] < inf, True, msg="{0}, {1}".format("weight of edge a b less than inf", W[a][b]))
+        self.assertEqual(W[c][e] < inf, False, msg="{0}, {1}".format("weight of edge a b less than inf", W[c][e]))
+        degree_node_a = sum(1 for w in W[a] if w < inf and w != 0)
+        self.assertEqual(degree_node_a, 5, msg="{0}, {1}".format("degree of node a", degree_node_a))
+
+
 if __name__ == '__main__':
     logging.basicConfig(stream=sys.stderr)
     logging.getLogger("Ch02TestSuite").setLevel(logging.DEBUG)
