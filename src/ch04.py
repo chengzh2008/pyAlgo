de441f649cd4145f0079ffc581bee98d0cad6f74
Zhihong Cheng <chengzh2008@gmail.com>
2017-09-30 21:54:11 -0700

    finish ch04

--- src/ch04.py
+++ src/ch04.py
@@ -1,4 +1,6 @@
 """Ch04"""
+from collections import defaultdict
+
 
 def cover(board, label=1, top=0, left=0, side=None):
     """cover board with a missing corner"""
@@ -54,3 +56,133 @@ def ins_sort_iter(seq):
             seq[j-1], seq[j] = seq[j], seq[j-1]
             j -= 1
     return seq
+
+def sel_sort_rec(seq, i):
+    """selection sort: recursive"""
+    if not i:
+        return seq
+    max = i
+    for j in range(i):
+        if seq[j] > seq[max]:
+            max = j
+    seq[max], seq[i] = seq[i], seq[max]
+    sel_sort_rec(seq, i - 1)
+    return seq
+
+def sel_sort_iter(seq):
+    for i in range(len(seq) - 1, 0, -1):
+        max = i
+        for j in range(i):
+            if seq[j] > seq[max]:
+                max = j
+        seq[max], seq[i] = seq[i], seq[max]
+    return seq
+
+def naive_max_permutation(M, A=None):
+    """naive implementation for maximum permutation
+    M = [2, 2, 0, 5, 3, 5, 7, 4]
+    #c is mapped to #a
+    """
+    if A is None:
+        A = set(range(len(M))) # set of the index
+    if len(A) == 1: # base case: single element
+        return A
+    B = set(M[i] for i in A) # set of the pointed index
+    C = A - B # set of the index that nobody is pointing to
+    if C:
+        A.remove(C.pop()) # remove index that nobody is pointing to
+        return naive_max_permutation(M, A)
+    return A
+
+def max_permutation(M):
+    n = len(M)
+    A = set(range(n)) # result set: initialized to be a full set
+    count = [0]*n # counter
+    for i in M:
+        count[i] += 1 # initialize counter
+    Q = [i for i in A if count[i] == 0]
+    while Q:
+        k = Q.pop()
+        A.remove(k)
+        j = M[k] # who is k pointing to
+        count[j] -= 1
+        if count[j] == 0:
+            Q.append(j)
+    return A
+
+def counting_sort(A, key=lambda x: x):
+    B, C = [], defaultdict(list)
+    for i in A:
+        C[key(i)].append(i)
+    for k in range(min(C), max(C) + 1):
+        B.extend(C[k])
+    return B
+
+def celebrity(G):
+    """ liner time to find a celebrity
+    who does not know anyone but everyone else knows him/her
+    """
+    number = len(G)
+    assert number >= 2
+    cand1, cand2 = 0, 1  # cand1, cand2 as potential candidate
+    for cand in range(2, number):
+        if G[cand1][cand2]:
+            cand1 = cand
+        else:
+            cand2 = cand
+    if cand1 == number: # cand1 was last replaced
+        cand = cand2
+    else:
+        cand = cand1
+    for everyone in range(number):
+        if everyone == cand:
+            continue
+        if G[cand][everyone]:
+            break # cand knows everyone
+        if not G[everyone][cand]:
+            break # someone does not know cand
+    else:
+        return cand # celebrity cand has been found
+    return None
+
+def naive_top_sort(G, S=None):
+    """naive implementation of topological sorting
+    similar to insertion sort
+    """
+    if S is None:
+        S = set(G) # put all items into the set
+    if len(S) == 1:
+        return list(S) # base: single item
+    take_one = S.pop() # reduction: take one out
+    seq = naive_top_sort(G, S) # recursively sort the rest
+    min_pos = 0 # mark for the position
+    for i, u in enumerate(seq):
+        if take_one in G[u]: # if there is an edge from i to take_one
+            min_pos = i + 1 # update the insert position
+    seq.insert(min_pos, take_one) # insert take_one to the right place
+    return seq
+
+def top_sort(G):
+    """topological sorting based on in-edge counting"""
+    count = dict((u, 0) for u in G) # initialize count 0 for every node
+    S = [] # result list
+    for u in G:
+        for v in G[u]:
+            count[v] += 1
+    Q = [i for i in G if count[i] == 0]
+    while Q:
+        k = Q.pop()
+        S.append(k)
+        for l in G[k]:
+            count[l] -= 1
+            if count[l] == 0:
+                Q.append(l)
+    return S
+
+def relexation():
+    """relexation technique"""
+    for v in range(n):
+        C[v] = float('inf')
+    for i in range(N): # N is a large number
+        u, v = random.randrange(n), random.randrange(n)
+        C[v] = min(C[v], A[u] + B[u][v]) # relax
--- tests/test_ch04.py
+++ tests/test_ch04.py
@@ -5,7 +5,10 @@ import random
 import unittest
 from copy import deepcopy
 
-from src.ch04 import cover, ins_sort_iter, ins_sort_rec, print_board
+from src.ch04 import (celebrity, counting_sort, cover, ins_sort_iter,
+                      ins_sort_rec, max_permutation, naive_max_permutation,
+                      naive_top_sort, print_board, sel_sort_iter, sel_sort_rec,
+                      top_sort)
 
 LOG = logging.getLogger(__name__)
 
@@ -22,14 +25,72 @@ class Ch04TestSuite(unittest.TestCase):
         self.assertEqual(board[7][0], 15)
 
     def test_rec_ins_sort(self):
+        LOG.info("Testing recursive insertion sort")
         seq = [random.randrange(1000) for i in range(100)]
         seq1 = deepcopy(seq)
         self.assertEqual(sorted(seq), ins_sort_rec(seq1, len(seq1) - 1))
 
     def test_iter_ins_sort(self):
+        LOG.info("Testing iterative insertion sort")
         seq = [random.randrange(1000) for i in range(100)]
         seq1 = deepcopy(seq)
         self.assertEqual(sorted(seq), ins_sort_iter(seq1))
+
+    def test_rec_sel_sort(self):
+        LOG.info("Testing recursive selection sort")
+        seq = [random.randrange(1000) for i in range(100)]
+        seq1 = deepcopy(seq)
+        self.assertEqual(sorted(seq), sel_sort_rec(seq1, len(seq1) - 1))
+
+    def test_iter_sel_sort(self):
+        LOG.info("Testing iterative selection sort")
+        seq = [random.randrange(1000) for i in range(100)]
+        seq1 = deepcopy(seq)
+        self.assertEqual(sorted(seq), sel_sort_iter(seq1))
+
+    def test_naive_max_perm(self):
+        M = [2, 2, 0, 5, 3, 5, 7, 4]
+        max_perm = {0, 2, 5}
+        real_result = naive_max_permutation(M)
+        self.assertEqual(real_result, max_perm, msg="{0} is not {1}".format(real_result, max_perm))
+
+    def test_max_perm(self):
+        M = [2, 2, 0, 5, 3, 5, 7, 4]
+        max_perm = {0, 2, 5}
+        real_result = max_permutation(M)
+        self.assertEqual(real_result, max_perm, msg="{0} is not {1}".format(real_result, max_perm))
+
+    def test_counting_sort(self):
+        LOG.info("Testing counting sort")
+        seq = [random.randrange(1000) for i in range(100)]
+        seq1 = deepcopy(seq)
+        self.assertEqual(sorted(seq), counting_sort(seq1))
+
+    def test_celebrity(self):
+        """
+        n = 100
+        G = [[random.randrange(2) for i in range(n)] for i in range(n)]
+        c = random.randrange(100) # choose a celebrity
+        for i in range(n): # make sure c does not know anyone else
+            G[i][c] = True # and anyone does know c
+            G[c][i] = False
+        found = celebrity(G)
+        self.assertEqual(c, found, msg="real celebrity {0}: the one found is {1} ".format(c, found))
+        """
+
+    def test_naive_top_sort(self):
+        G = {
+            'a': {'b', 'f'},
+            'b': {'c', 'd', 'f'},
+            'c': {'d'},
+            'd': {'e', 'f'},
+            'e': {'f'},
+            'f': {}
+        }
+        real_sort = ['a', 'b', 'c', 'd', 'e', 'f']
+        self.assertEqual(real_sort, top_sort(G))
+        self.assertEqual(naive_top_sort(G), top_sort(G))
+
 
 if __name__ == '__main__':
     unittest.main()
